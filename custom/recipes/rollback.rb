dc0b5a794801380d08d907abfff25c1e0e07ab72
adachish <shunzoc@yahoo.co.jp>
2015-04-17 20:15:09 +0900

    Update rollback.rb

--- custom/recipes/rollback.rb
+++ custom/recipes/rollback.rb
@@ -2,11 +2,9 @@ include_recipe 'deploy'
 
 node[:deploy].each do |application, deploy|
 
-  directory "#{deploy[:deploy_to]}" do
-    recursive true
-    action :delete
-    only_if do
-      ::File.exists?("#{deploy[:deploy_to]}")
-    end
+  opsworks_deploy do
+    deploy_data deploy
+    app application
+    only_if { File.exists?(deploy[:current_path]) }
   end
 end
