f563809d473a4499a1fa694498012d75ffaa3759
adachish <shunzoc@yahoo.co.jp>
2015-04-18 02:45:54 +0900

    Update rollback.rb

--- custom/recipes/rollback.rb
+++ custom/recipes/rollback.rb
@@ -3,9 +3,5 @@ node[:deploy].each do |application, deploy|
   opsworks_deploy do
     user deploy[:user]
     action :rollback
-    
-    only_if do
-      File.exists?(deploy[:current_path])
-    end
   end
 end
