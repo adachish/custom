d842fc34dab42a6064369bc412022e49ba1b3e83
adachish <shunzoc@yahoo.co.jp>
2015-04-21 14:40:42 +0900

    Update default.rb

--- custom/attributes/default.rb
+++ custom/attributes/default.rb
@@ -3,4 +3,6 @@
 
 default[:opsworks][:deploy_user][:user] = 'ec2-user'
 default[:opsworks][:deploy_user][:group] = 'ec2-user'
-default[:deploy][application][:deploy_to] = "/var/www/#{application}"
+node[:deploy].each do |application, deploy|
+  default[:deploy][application][:deploy_to] = "/var/www/#{application}"
+end
