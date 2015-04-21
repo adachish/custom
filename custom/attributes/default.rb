995ed2f742fbd8b57a3729bf9d7031b5cbc4cc54
adachish <shunzoc@yahoo.co.jp>
2015-04-21 14:47:50 +0900

    Update default.rb

--- custom/attributes/default.rb
+++ custom/attributes/default.rb
@@ -1,8 +1,8 @@
 #default['deploy']['hoge']['user']='root' 
 #default['deploy']['hoge']['group']='ec2-user' 
 
-default[:opsworks][:deploy_user][:user] = 'ec2-user'
-default[:opsworks][:deploy_user][:group] = 'ec2-user'
+normal[:opsworks][:deploy_user][:user] = 'ec2-user'
+normal[:opsworks][:deploy_user][:group] = 'ec2-user'
 node[:deploy].each do |application, deploy|
-  default[:deploy][application][:deploy_to] = "/var/www/#{application}"
+  normal[:deploy][application][:deploy_to] = "/var/www/#{application}"
 end
