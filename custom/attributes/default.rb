68d98d788528c83efde04a097df42a24efb10a2f
adachish <shunzoc@yahoo.co.jp>
2015-04-21 14:17:47 +0900

    Update default.rb

--- custom/attributes/default.rb
+++ custom/attributes/default.rb
@@ -1,3 +1,6 @@
-default['deploy']['hoge']['user']='root' 
-default['deploy']['hoge']['group']='ec2-user' 
+#default['deploy']['hoge']['user']='root' 
+#default['deploy']['hoge']['group']='ec2-user' 
 
+default[:opsworks][:deploy_user][:user] = 'ec2-user'
+default[:opsworks][:deploy_user][:group] = 'ec2-user'
+default[:deploy][application][:deploy_to] = "/var/www/#{application}"
